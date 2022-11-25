3e4e5d0c2193fc1b278f36a2f2da16db7439a6ba
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2022-11-25 22:06:02 +0100

    test: Added test for environment endpoint

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,24 +2,20 @@ from copy import deepcopy
 
 import pytest
 from data_organizer.db.connection import DatabaseConnection
+from dynaconf import LazySettings
 from fastapi.testclient import TestClient
 from sqlalchemy import text
-from utils import generate_token
 
 import iot_data_receiver
 from iot_data_receiver.main import app
+from iot_data_receiver.utils import generate_token
 
 
-@pytest.fixture(scope="session")
-def test_session_token(session_mocker):
-    settings_ = deepcopy(iot_data_receiver.main.config.settings)
-
-    settings_.db.schema = "iot_receiver_test"
-    token, hashed_token = generate_token(20)
-
-    session_mocker.patch.object(iot_data_receiver.main.config, "settings", settings_)
-
-    db = DatabaseConnection(**settings_.db.to_dict(), name="IoTReceiver_test")
+def setup_database(
+    db: DatabaseConnection,
+    settings_: LazySettings,
+    hashed_token: str,
+):
     with db.engine.connect() as connection:
         connection.execute(
             text(
@@ -55,13 +51,103 @@ def test_session_token(session_mocker):
                 """
             )
         )
+
         connection.commit()
+
+
+def drop_schema(db: DatabaseConnection, schema: str):
+    with db.engine.connect() as connection:
+        connection.execute(text(f"DROP SCHEMA {schema} CASCADE"))
+        connection.commit()
+
+
+def mock_settings(mocker):
+    settings_ = deepcopy(iot_data_receiver.main.config.settings)
+
+    settings_.db.schema = "iot_receiver_test"
+
+    mocker.patch.object(iot_data_receiver.main.config, "settings", settings_)
+
+    return settings_
+
+
+@pytest.fixture
+def test_session(mocker):
+    settings_ = mock_settings(mocker)
+
+    token, hashed_token = generate_token(20)
+
+    db = DatabaseConnection(**settings_.db.to_dict(), name="IoTReceiver_test")
+    setup_database(db, settings_, hashed_token)
+
+    yield token, db
+
+    drop_schema(db, settings_.db.schema)
+
+
+@pytest.fixture
+def test_environment_session_full(mocker):
+    settings_ = mock_settings(mocker)
+
+    token, hashed_token = generate_token(20)
+
+    db = DatabaseConnection(**settings_.db.to_dict(), name="IoTReceiver_test")
+    setup_database(db, settings_, hashed_token)
+
+    with db.engine.connect() as connection:
+        table_name = "test_name_environment"
+        connection.execute(
+            text(
+                """
+                INSERT INTO %s.endpoint_request_subsets
+                VALUES (
+                    1,
+                    'environment',
+                    '%s',
+                    '{"fields": ["timestamp", "temperature", "pressure", "humidity",
+                                 "light", "gas_ox", "gas_red", "gas_nh3"]}'
+                );
+                """
+                % (settings_.db.schema, table_name)
+            )
+        )
+        connection.execute(
+            text(
+                f"""
+                CREATE TABLE {settings_.db.schema}.test_name_environment (
+                    timestamp timestamp without time zone PRIMARY KEY,
+                    temperature double precision NOT NULL,
+                    pressure double precision NOT NULL,
+                    humidity double precision NOT NULL,
+                    light double precision NOT NULL,
+                    gas_ox double precision NOT NULL,
+                    gas_red double precision NOT NULL,
+                    gas_nh3 double precision NOT NULL
+                );
+                """
+            )
+        )
+        connection.commit()
+
+    yield token, db
+
+    drop_schema(db, settings_.db.schema)
+
+
+@pytest.fixture
+def test_registered_session(mocker):
+    settings_ = mock_settings(mocker)
+
+    token, hashed_token = generate_token(20)
+
+    db = DatabaseConnection(
+        **settings_.db.to_dict(), name="IoTReceiver_registered_test"
+    )
+    setup_database(db, settings_, hashed_token)
 
     yield token
 
-    with db.engine.connect() as connection:
-        connection.execute(text(f"DROP SCHEMA {settings_.db.schema} CASCADE"))
-        connection.commit()
+    drop_schema(db, settings_.db.schema)
 
 
 @pytest.fixture
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,22 +1,72 @@
+from datetime import datetime
+
 import pytest
+from data_organizer.db.connection import DatabaseConnection
 
 
-def test_health(test_session_token, client):
+def test_health(test_session, client):
     response = client.get("/health")
     assert response.status_code == 200
 
 
 @pytest.mark.parametrize("header", [None, {"access_token": "boguskey"}])
-def test_register_invalid_key(test_session_token, client, header):
+def test_register_invalid_key(test_session, client, header):
     response = client.post("/register", headers=header)
     assert response.status_code == 403
 
 
-def test_register(test_session_token, client):
+def test_register(test_session, client):
+    key, db = test_session
     response = client.post(
         "/register",
         json={"endpoint": "environment", "fields": []},
-        headers={"access_token": test_session_token},
+        headers={"access_token": key},
     )
 
     assert response.status_code == 200
+    assert "Successfully registered" in response.text
+
+    response = client.post(
+        "/register",
+        json={"endpoint": "environment", "fields": []},
+        headers={"access_token": key},
+    )
+
+    assert response.status_code == 200
+    assert "Endpoint already registered for" in response.text
+
+
+def get_data(db: DatabaseConnection, table: str, time_stamp: str):
+    return db.query_to_df(
+        f"""
+        SELECT *
+        FROM {table}
+        WHERE timestamp = '{time_stamp}'
+        """
+    )
+
+
+def test_environment(test_environment_session_full, client):
+    key, db = test_environment_session_full
+
+    time_stamp = datetime.now().isoformat()
+    response = client.post(
+        "/environment",
+        json={
+            "timestamp": [time_stamp],
+            "temperature": [1.1],
+            "pressure": [2.2],
+            "humidity": [3.3],
+            "light": [4.4],
+            "gas_ox": [5.5],
+            "gas_red": [6.6],
+            "gas_nh3": [7.7],
+        },
+        headers={"access_token": key},
+    )
+
+    assert response.status_code == 200
+
+    data = get_data(db, "test_name_environment", time_stamp)
+
+    assert not data.empty
