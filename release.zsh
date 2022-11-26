a488a02c2e1b1f7229174fa7f7312bd651d64a22
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2022-11-26 21:46:18 +0100

    build: Added release script

---
+++ release.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+echo "Bumping Version"
+new_version=$(poetry version --dry-run --short $1)
+bump_msg=$(poetry version $1)
+
+echo "Updating changelog"
+conventional-changelog -p conventionalcommits -i CHANGELOG.md -s
+if [[ $? != 0 ]]
+then
+  echo "Generating changelog failed. Exiting..."
+  exit 1
+fi
+
+git add CHANGELOG.md pyproject.toml
+git commit -n -m "${bump_msg}"
+git tag ${new_version}
+
+echo "Pushing"
+git push
+echo "Pushing tag"
+git push --tag
